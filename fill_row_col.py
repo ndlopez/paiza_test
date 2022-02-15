4ee1727c4c09e6deb543239383c3aa6ea617786e
ndlopez <ndzerglink@live.jp>
2022-02-15 10:53:17 +0900

    Created a function to print the array

---
+++ fill_row_col.py
@@ -0,0 +1,26 @@
+#input array
+bomb = [[".",".","#","."],[".",".",".","."],[".",".",".","."],[".",".",".","."]]
+position= []
+for i in range(len(bomb)):
+    for j in range(len(bomb[i])):
+        if bomb[i][j] == "#":
+               position.append(i)
+               position.append(j)
+        print(bomb[i][j],end = " ")
+    print()
+
+print("# position:",position)
+count=0
+#Fill the row with "x"
+for i in range(0,len(position),2):
+    for j in range(len(bomb[position[0]])):
+        bomb[position[i]][j] = "x"
+        count = count + 1
+#Fill the column with "x"
+for j in range(1,len(position),2):
+    for i in range(len(bomb[position[1]])):
+        bomb[i][position[j]] = "x"
+        if i == position[j]:
+            count = count - 1
+        count = count + 1
+print(bomb,count)
