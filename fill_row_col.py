2966db80939346ff2c4c868e3bd7d51da9986c53
ndlopez <ndzerglink@live.jp>
2022-02-15 11:46:21 +0900

    Added a new function to search for chars

--- fill_row_col.py
+++ fill_row_col.py
@@ -1,34 +1,35 @@
 #input array
-bomb = [["#",".","#","."],[".",".",".","."],[".",".",".","."],[".",".","#","."]]
+bomb = [["#",".",".","."],[".",".",".","."],[".",".",".","."],[".",".",".","."]]
 position= []
 def print_arr():
     for i in range(len(bomb)):
         for j in range(len(bomb[i])):
             print(bomb[i][j],end=" ")
         print()
+#Search for "#" char, save position and count 
+def find_char(myChar):
+    kount=0
+    for i in range(len(bomb)):
+        for j in range(len(bomb[i])):
+            if bomb[i][j] == myChar:
+                position.append(i)
+                position.append(j)
+                kount=kount+1
+    return kount
 
-for i in range(len(bomb)):
-    for j in range(len(bomb[i])):
-        if bomb[i][j] == "#":
-               position.append(i)
-               position.append(j)
-
+print_arr()
+print("#s: ",find_char("#"))
 print("# position:",position)
-count=0
 #Fill the row with "x"
 for i in range(0,len(position),2):
     for j in range(len(bomb[position[0]])):
         bomb[position[i]][j] = "x"
-        if j == position[i]:
-            count = count - 1
-        count = count + 1
 
 #Fill the column with "x"
 for j in range(1,len(position),2):
     for i in range(len(bomb[position[1]])):
         bomb[i][position[j]] = "x"
-        if i == position[j]:
-            count = count - 1
-        count = count + 1
+
 print_arr()
-print(count)
+
+print("count",find_char("x"))
