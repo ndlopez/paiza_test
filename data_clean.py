87947256c0e6771c8455b3a6bfd1e33837922d9b
ndlopez <ndzerglink@live.jp>
2022-02-16 16:34:24 +0900

    Updated

--- data_clean.py
+++ data_clean.py
@@ -22,38 +22,29 @@ def print_arr():
 
 #Find elem in data <> number
 #if elem <0 or elem = str: then ignore
-sum,aux,vec=[],[],[]
+sum,aux=[],[]
+kount,count=[],[]
 for i in range(int(num_questions)):
     sum.append(0)
     aux.append(0)
-    vec.append(0)
-kount=[]
+    kount.append(0)
+    count.append(0)
+#kount=[]
 #print(sum,aux)
 for i in range(len(data)):
     for j in range(len(data[i])):
-        if not data[i][j].isnumeric():
-            data[i][j]="_"
-            kount.append(j)
-            
-        else:
-            sum[j]=aux[j]+int(data[i][j])
-            aux[j]=sum[j]
+        if data[i][j].isnumeric():
+            if int(data[i][j]) < 101:
+                sum[j]=aux[j]+int(data[i][j])
+                aux[j]=sum[j]
+                count[j]=count[j]+1
+                kount[j]=count[j]
+        #else:
+        #    data[i][j]="_"
                         
 #print("Sort of cleaned array")
 print(kount)
-count=0
-for i in range(len(kount)):
-    for j in range(len(sum)):
-        aux[j],count=0,0
-        if kount[i] == j:
-            count=count+1
-            vec[j]=aux[j]+count
-            aux[j]=vec[j]
-        
-#print_arr()
-for j in range(len(sum)):
-    if kount[j] == j:
-        print(sum[j]/(int(num_people)-kount[0]))
-    else:
-        print(sum[j]/int(num_people))
+
 #Average remaning elements of column
+for j in range(len(sum)):
+    print(int(sum[j]/kount[j]))
